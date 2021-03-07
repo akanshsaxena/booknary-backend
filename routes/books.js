90b4d03fcf2d2b8c6fab96f9fcf92c8677cfa845
unknown <53352640+akanshsaxena@users.noreply.github.com>
2021-03-07 22:38:30 +0530

    added search api endpoint

--- routes/books.js
+++ routes/books.js
@@ -1,11 +1,18 @@
 const router = require("express").Router();
-const { validatePost } = require("../joi_validation/Books");
+const {
+  validatePost
+} = require("../joi_validation/Books");
 const verifyToken = require("../verifyToken");
 const Book = require("../model/Books");
 const Video = require("../model/Video");
 
+function escapeRegex(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
 router.post("/upload", async (req, res) => {
-  const { error } = validatePost(req.body);
+  const {
+    error
+  } = validatePost(req.body);
   if (error) {
     console.log(error);
     return res.send(error.details[0].message);
@@ -24,38 +31,78 @@ router.post("/upload", async (req, res) => {
   try {
     const savedBook = await book.save();
     console.log("uploaded new book");
-    res.send({message: "Success"});
+    res.send({
+      message: "Success"
+    });
   } catch (err) {
     res.send(err);
   }
 });
-
+// router.get("/trial/", async (req, res) => {
+//   try {
+//     const {
+//       trial
+//     } = req.query;
+//     if (trial) {
+//       console.log(trial);
+//     } else {
+//       console.log("empty trial");
+//     }
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
 router.get("/get/", async (req, res) => {
   try {
-    const { category, language, authorId, bookId, searchText } = req.query;
-    if (category === "all") {
-      if (!authorId.includes("null")) {
-        console.log("searched in author id");
-        const getAdv = await Book.find({
-          authorId: authorId,
-        });
-        res.send(getAdv);
-      } else if (!bookId.includes("null")) {
-        console.log("searched in book id");
-        const getAdv = await Book.find({
-          _id: bookId,
-        });
-        res.send(getAdv);
-      } else if (!searchText.includes("0")) {
+    const {
+      category,
+      language,
+      authorId,
+      bookId,
+      searchText
+    } = req.query;
+    if (category) {
+      if (category === "All") {
         console.log("search insearch text");
         const getAdv = await Book.find({});
-        res.send(getAdv);
+        if (language === "All") {
+          res.send(getAdv);
+        } else {
+          res.send(getAdv.filter(book => book.language === language));
+        }
+      } else {
+        console.log("category searched");
+        const getAdv = await Book.find({
+          category: category,
+        });
+        if (language === "All") {
+          res.send(getAdv);
+        } else {
+          res.send(getAdv.filter(book => book.language === language));
+        }
       }
-    } else {
-      console.log("category searched");
+    } else if (authorId) {
+      console.log("searched in author id");
       const getAdv = await Book.find({
-        category: category,
+        authorId: authorId,
       });
+      res.send(getAdv);
+    } else if (bookId) {
+      console.log("searched in book id");
+      const getAdv = await Book.find({
+        _id: bookId,
+      });
+      res.send(getAdv);
+    } else if (searchText) {
+      const regex = new RegExp(escapeRegex(searchText), 'gi');
+      console.log("search insearch text " + regex);
+      const getAdv1 = await Book.find({
+        author: regex
+      });
+      const getAdv2 = await Book.find({
+        title: regex
+      });
+      const getAdv = [...getAdv1, ...getAdv2];
       res.send(getAdv);
     }
   } catch (err) {
@@ -65,19 +112,18 @@ router.get("/get/", async (req, res) => {
 
 router.post("/updateVotes/", async (req, res) => {
   try {
-    const { _id } = req.query;
+    const {
+      _id
+    } = req.query;
     const video = await Video.findOne({
       _id: _id,
     });
     let views = video.views;
-    const updatedBook = await Video.updateOne(
-      {
-        _id: _id,
-      },
-      {
-        views: views + 1,
-      }
-    );
+    const updatedBook = await Video.updateOne({
+      _id: _id,
+    }, {
+      views: views + 1,
+    });
     if (updatedBook.ok) {
       console.log("updated vote");
       res.send(updatedBook);
@@ -97,14 +143,11 @@ router.post("/votes", async (req, res) => {
     });
     let votes = parseInt(book.votes);
     console.log(votes);
-    const updatedBook = await Book.updateOne(
-      {
-        _id: bookId,
-      },
-      {
-        votes: votes + 1,
-      }
-    );
+    const updatedBook = await Book.updateOne({
+      _id: bookId,
+    }, {
+      votes: votes + 1,
+    });
     if (updatedBook.ok) {
       console.log("updated vote");
       res.send(updatedBook);
